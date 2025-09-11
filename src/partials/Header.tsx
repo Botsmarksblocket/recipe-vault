8eafdfec3228df475e41e7a3110f3700feac08e3
Edvard Andersson <edvard.andersson.syne24lin@edu.tucsweden.se>
2025-09-11 10:58:50 +0300

    Started working on the header. Added Search-bar.

--- src/partials/Header.tsx
+++ src/partials/Header.tsx
@@ -1,10 +1,9 @@
-import { useState } from 'react';
-import { Link, useLocation } from 'react-router-dom';
-import { Container, Nav, Navbar } from 'react-bootstrap';
-import routes from '../routes';
+import { useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+import { Container, Nav, Navbar, Form, Button } from "react-bootstrap";
+import routes from "../routes";
 
 export default function Header() {
-
   // whether the navbar is expanded or not
   // (we use this to close it after a click/selection)
   const [expanded, setExpanded] = useState(false);
@@ -12,39 +11,57 @@ export default function Header() {
   //  get the current route
   const pathName = useLocation().pathname;
   const currentRoute = routes
-    .slice().sort((a, b) => a.path.length > b.path.length ? -1 : 1)
-    .find(x => pathName.indexOf(x.path.split(':')[0]) === 0);
+    .slice()
+    .sort((a, b) => (a.path.length > b.path.length ? -1 : 1))
+    .find((x) => pathName.indexOf(x.path.split(":")[0]) === 0);
   // function that returns true if a menu item is 'active'
   const isActive = (path: string) =>
     path === currentRoute?.path || path === currentRoute?.parent;
 
-  return <header>
-    <Navbar
-      expanded={expanded}
-      expand="md"
-      className="bg-primary"
-      data-bs-theme="dark"
-      fixed="top"
-    >
-      <Container fluid>
-        <Navbar.Brand className="me-5" as={Link} to="/">
-          The Good Grocery
-        </Navbar.Brand>
-        <Navbar.Toggle onClick={() => setExpanded(!expanded)} />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            {routes.filter(x => x.menuLabel).map(
-              ({ menuLabel, path }, i) =>
-                <Nav.Link
-                  as={Link} key={i} to={path}
-                  className={isActive(path) ? 'active' : ''}
-                  /* close menu after selection*/
-                  onClick={() => setTimeout(() => setExpanded(false), 200)}
-                >{menuLabel}</Nav.Link>
-            )}
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  </header>;
+  return (
+    <header>
+      <Navbar
+        expand="lg"
+        className="bg-body-tertiary"
+        expanded={expanded}
+        fixed="top"
+        data-bs-theme="dark"
+      >
+        <Container>
+          <Navbar.Brand as={Link} to="/">
+            Recipe vault
+          </Navbar.Brand>
+
+          <Navbar.Toggle onClick={() => setExpanded(!expanded)} />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              {routes
+                .filter((x) => x.menuLabel)
+                .map(({ menuLabel, path }, i) => (
+                  <Nav.Link
+                    as={Link}
+                    key={i}
+                    to={path}
+                    className={isActive(path) ? "active" : ""}
+                    /* close menu after selection*/
+                    onClick={() => setTimeout(() => setExpanded(false), 200)}
+                  >
+                    {menuLabel}
+                  </Nav.Link>
+                ))}
+              <Form className="d-flex">
+                <Form.Control
+                  type="search"
+                  placeholder="Search"
+                  className="me-2"
+                  aria-label="Search"
+                />
+                <Button variant="outline-success">Search</Button>
+              </Form>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
 }
