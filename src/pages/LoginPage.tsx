19b696ee190796724f45f8015220ed1c5e5a4caa
Edvard Andersson <edvard.andersson.syne24lin@edu.tucsweden.se>
2025-09-22 14:24:56 +0300

    Finished frontend for login page

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,4 +1,4 @@
-import { Row, Col, Card, Form } from "react-bootstrap";
+import { Row, Col, Card, Form, Button } from "react-bootstrap";
 
 LoginPage.route = {
   path: "/login",
@@ -8,18 +8,28 @@ export default function LoginPage() {
   return (
     <>
       <Row>
-        <Col>
-          <Card>
+        <Col className="d-flex justify-content-center mt-5">
+          <Card className="mt-5" style={{ width: "30rem" }}>
             <Card.Body>
               <Card.Title className="fs-1">Log in</Card.Title>
               <Form>
                 <Form.Group>
-                  <Form.Label>Username</Form.Label>
-                  <Form.Control placeholder="username"></Form.Control>
+                  <Form.Label className="d-block mt-3">Username</Form.Label>
+                  <Form.Control
+                    placeholder="Enter username..."
+                    className="d-block mt-2"
+                  ></Form.Control>
                 </Form.Group>
                 <Form.Group>
-                  <Form.Label>Password</Form.Label>
-                  <Form.Control placeholder="password"></Form.Control>
+                  <Form.Label className="d-block mt-2">Password</Form.Label>
+                  <Form.Control
+                    className="mt-2"
+                    placeholder="Enter password..."
+                  ></Form.Control>
+
+                  <div className="d-grid gap-2">
+                    <Button className="mt-4">Log in</Button>
+                  </div>
                 </Form.Group>
               </Form>
             </Card.Body>
