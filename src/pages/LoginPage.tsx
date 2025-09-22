075f076b42f5e93fc297ec04efb1745a0efb8710
Edvard Andersson <edvard.andersson.syne24lin@edu.tucsweden.se>
2025-09-22 12:52:35 +0300

    Created the skeleton for the login-page

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,7 +1,31 @@
+import { Row, Col, Card, Form } from "react-bootstrap";
+
 LoginPage.route = {
   path: "/login",
 };
 
 export default function LoginPage() {
-  return <></>;
+  return (
+    <>
+      <Row>
+        <Col>
+          <Card>
+            <Card.Body>
+              <Card.Title className="fs-1">Log in</Card.Title>
+              <Form>
+                <Form.Group>
+                  <Form.Label>Username</Form.Label>
+                  <Form.Control placeholder="username"></Form.Control>
+                </Form.Group>
+                <Form.Group>
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control placeholder="password"></Form.Control>
+                </Form.Group>
+              </Form>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </>
+  );
 }
