b2b2cda87a0e9f43e3eab32735678e29fdb1a50e
Edvard Andersson <edvard.andersson.syne24lin@edu.tucsweden.se>
2025-09-22 16:03:44 +0300

    Made it possible to log in. Not secure yet.

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,10 +1,37 @@
+import { useState } from "react";
 import { Row, Col, Card, Form, Button } from "react-bootstrap";
 
 LoginPage.route = {
   path: "/login",
 };
-
 export default function LoginPage() {
+  const [user, setUser] = useState<User>({
+    email: "",
+    password: "",
+  });
+
+  interface User {
+    email: string;
+    password: string;
+  }
+
+  function setProperty(event: React.ChangeEvent) {
+    event.preventDefault();
+    let { name, value }: { name: string; value: string } =
+      event.target as HTMLInputElement;
+
+    setUser({ ...user, [name]: value });
+  }
+
+  async function sendForm(event: React.FormEvent) {
+    event.preventDefault();
+    await fetch("/api/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(user),
+    });
+  }
+
   return (
     <>
       <Row>
@@ -12,23 +39,31 @@ export default function LoginPage() {
           <Card className="mt-5" style={{ width: "30rem" }}>
             <Card.Body>
               <Card.Title className="fs-1">Log in</Card.Title>
-              <Form>
+              <Form onSubmit={sendForm}>
                 <Form.Group>
                   <Form.Label className="d-block mt-3">Username</Form.Label>
                   <Form.Control
-                    placeholder="Enter username..."
+                    onChange={setProperty}
+                    type="text"
+                    name="email"
+                    placeholder="Enter email..."
                     className="d-block mt-2"
                   ></Form.Control>
                 </Form.Group>
                 <Form.Group>
                   <Form.Label className="d-block mt-2">Password</Form.Label>
                   <Form.Control
+                    onChange={setProperty}
+                    type="text"
+                    name="password"
                     className="mt-2"
                     placeholder="Enter password..."
                   ></Form.Control>
 
                   <div className="d-grid gap-2">
-                    <Button className="mt-4">Log in</Button>
+                    <Button className="mt-4" type="submit">
+                      Log in
+                    </Button>
                   </div>
                 </Form.Group>
               </Form>
