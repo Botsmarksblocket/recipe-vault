97721ec0313bb8692e09cd9c0484d3dc3db08e3e
Edvard Andersson <edvard.andersson.syne24lin@edu.tucsweden.se>
2025-09-23 13:45:56 +0300

    Created the skeleton for the  AuthProvider. Created function for user to log in.

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useLocation } from "react-router-dom";
 import Header from "./partials/Header";
 import Main from "./partials/Main";
 import Footer from "./partials/Footer";
-
+import { AuthProvider } from "./context/AuthProvider";
 
 export default function App() {
   // scroll to top when the route changes
@@ -11,9 +11,11 @@ export default function App() {
 
   return (
     <>
-      <Header />
-      <Main />
-      <Footer />
+      <AuthProvider>
+        <Header />
+        <Main />
+        <Footer />
+      </AuthProvider>
     </>
   );
 }
--- src/context/AuthProvider.tsx
+++ src/context/AuthProvider.tsx
@@ -1,9 +1,65 @@
-import { createContext} from "react";
+import { type ReactNode, createContext, useState, useEffect } from "react";
 import type User from "../interfaces/User";
 
 interface AuthContextType {
   user: User | null;
-  loginUser: (email: string, password: string) => Promise<boolean>
+  loginUser: (email: string, password: string) => Promise<boolean>;
 }
 
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+//Provides authentication context to all children
+export function AuthProvider({ children }: { children: ReactNode }) {
+  const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
+  const fetchUser = async () => {
+    const response = await fetch("/api/login");
+    try {
+      if (response.ok) {
+        const user = await response.json();
+        setUser(user);
+      } else {
+        setUser(null);
+      }
+    } catch {
+      setUser(null);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const loginUser = async (email: string, password: string) => {
+    try {
+      const response = await fetch("/api/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ email, password }),
+      });
+
+      if (response.ok) {
+        await fetchUser();
+        return true;
+      } else {
+        setError("Incorrect email or password.");
+        return false;
+      }
+    } catch (error) {
+      console.log("Login failure", error);
+      return false;
+    }
+  };
+
+  //Fetches user after first render
+  useEffect(() => {
+    fetchUser().finally(() => setLoading(false));
+  }, []);
+
+  //Return context provider
+  return (
+    <AuthContext.Provider value={{ user, loginUser }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
