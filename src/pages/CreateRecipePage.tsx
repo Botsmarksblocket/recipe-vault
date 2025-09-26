2a11167bcd51a526ad4f03c494b884cafe26151e
Edvard Andersson <edvard.andersson.syne24lin@edu.tucsweden.se>
2025-09-26 09:27:31 +0300

    Set up CreateRecipePage for react router

---
+++ src/pages/CreateRecipePage.tsx
@@ -0,0 +1,9 @@
+CreateRecipePage.route = {
+  path: "/create-recipe",
+  menuLabel: "Create recipe",
+  requiresAuth: true,
+  index: 3,
+};
+export default function CreateRecipePage() {
+  return <></>;
+}
