a4a730e641ab91a16f18abdbbfae711df49896d6
Edvard Andersson <edvard.andersson.syne24lin@edu.tucsweden.se>
2025-10-02 11:04:17 +0300

    Started with the skeleton of the frontend for filtering

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,6 @@
 import type Recipe from "../interfaces/Recipe";
 import RecipeCard from "../components/RecipeCard";
-import { Row, Col } from "react-bootstrap";
+import { Row, Col, Card, Form } from "react-bootstrap";
 import { useLoaderData } from "react-router-dom";
 
 HomePage.route = {
@@ -26,6 +26,29 @@ export default function HomePage() {
           <h1 className="mx-sm-0 mt-xs-0 mt-3">Check out these recipes!</h1>
         </Col>
       </Row>
+
+      <Card className="mb-3 mt-3">
+        <Card.Title className="fs-3 ms-3 mt-2">Filter recipes</Card.Title>
+        <Card.Body>
+          <Form>
+            <Row className="d-flex justify-content-center">
+              <Col xs={6}>
+                <Form.Select>
+                  <option value="">Filter by meal type</option>
+                  <option value="typ1">Typ 1</option>
+                </Form.Select>
+              </Col>
+              <Col xs={6}>
+                <Form.Select>
+                  <option value="">Filter by creator</option>
+                  <option value="typ1">Typ 1</option>
+                </Form.Select>
+              </Col>
+            </Row>
+          </Form>
+        </Card.Body>
+      </Card>
+
       <Row>
         {recipes.map((recipe) => (
           <Col xs={12} sm={6} md={4} lg={3} className="mb-3" key={recipe.id}>
